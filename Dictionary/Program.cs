d2802b31ca9410ad02b179fd1f2e2cdf6b4c6932
MerBasNik <125216886+MerBasNik@users.noreply.github.com>
2025-05-01 21:34:10 +0300

    исправил ошибки

--- Dictionary/Program.cs
+++ Dictionary/Program.cs
@@ -1,105 +1,6 @@
-﻿class Dictionary
-{
-    static Dictionary<string, string> dictEngToRus = new Dictionary<string, string>();
-    static Dictionary<string, string> dictRusToEng = new Dictionary<string, string>();
-    public static void TranslateWord()
-    {
-        Console.Write("Введите слово для перевода: ");
-        string word = Console.ReadLine().Trim();
-        if (dictEngToRus.ContainsKey(word))
-        {
-            Console.WriteLine($"{word} --> {dictEngToRus[word]}");
-        }
-        else if (dictRusToEng.ContainsKey(word))
-        {
-            Console.WriteLine($"{word} --> {dictRusToEng[word]}");
-        }
-        else
-        {
-            Console.Write("Такого слова в словаре нет. Хотите его добавить? (да/нет): ");
-            string answer = Console.ReadLine();
-            if (answer.ToLower() == "да")
-            {
-                AddNewWord(word);
-            }
-        }
-    }
+﻿using Dictionary.Implementation;
 
-    public static void AddNewWord(string word = null)
-    {
-        if (word == null)
-        {
-            Console.Write("Введите слово которое хотите добавить: ");
-            word = Console.ReadLine().Trim();
-        }
-
-        if (!dictEngToRus.ContainsKey(word) && !dictRusToEng.ContainsKey(word))
-        {
-            Console.Write($"Введите перевод слова {word}: ");
-            string translateWord = Console.ReadLine().Trim();
-
-            string lang = WhatLanguage(translateWord);
-            if (lang == "ru")
-            {
-                dictEngToRus.Add(word, translateWord);
-                dictRusToEng.Add(translateWord, word);
-            }
-            else
-            {
-                dictRusToEng.Add(word, translateWord);
-                dictEngToRus.Add(translateWord, word);
-            }
-
-            Console.WriteLine($"Слово c переводом добавлено в словарь: {word} --> {translateWord}");
-        }
-        else
-        {
-            Console.WriteLine("Такое слово уже есть в словаре");
-        }
-    }
-
-    static string WhatLanguage(string word)
-    {
-        foreach (char ch in word)
-        {
-            if ((ch >= 'a' && ch <= 'z') || (ch >= 'A' && ch <= 'Z'))
-            {
-                return "en";
-                break;
-            }
-        }
-
-        return "ru";
-    }
-    
-    public static void LoadDictionary(string filePath)
-    {
-        string fileName = Path.GetFileName(filePath);
-        if (!File.Exists(filePath))
-        {
-            Console.WriteLine($"Файл {fileName} не существует");
-        }
-        List<string> dict = File.ReadLines(filePath).ToList();
-        foreach (string line in dict)
-        {
-            string[] pair = line.Split(':');
-            dictEngToRus.Add(pair[0], pair[1]);
-            dictRusToEng.Add(pair[1], pair[0]);
-        }
-    }
-
-    public static void SaveDictionary(string filePath)
-    {
-        List<string> words = new List<string>();
-        foreach (KeyValuePair<string, string> pair in dictEngToRus)
-        {
-            words.Add($"{pair.Key}:{pair.Value}");
-        }
-
-        File.WriteAllLines(filePath, words);
-        Console.WriteLine("Словарь сохранён");
-    }
-}
+namespace Dictionary;
 
 class Program
 {
@@ -107,8 +8,9 @@ class Program
     const string dictionaryFilePath = $"../../../{dictionaryFileName}";
     static void Main()
     {
+        
         Console.WriteLine("Словарь с переводом (английский/русский)");
-        Dictionary.LoadDictionary(dictionaryFilePath);
+        DictionaryImplement.LoadDictionary(dictionaryFilePath);
         bool isContinue = true;
 
         while (isContinue)
@@ -121,13 +23,13 @@ class Program
             switch (desicion)
             {
                 case "1":
-                    Dictionary.TranslateWord();
+                    DictionaryImplement.TranslateWord();
                     break;
                 case "2":
-                    Dictionary.AddNewWord();
+                    DictionaryImplement.AddNewWord();
                     break;
                 case "3":
-                    Dictionary.SaveDictionary(dictionaryFilePath);
+                    DictionaryImplement.SaveDictionary(dictionaryFilePath);
                     isContinue = false;
                     break;
                 default:
